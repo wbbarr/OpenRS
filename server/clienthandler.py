df04b7239f9894fb2f1d8ec995edfb32ba26547a
William Barr <william.barr@gatech.edu>
2011-08-15 23:51:54 -0400

    Finished off preliminary version of message receive code.

--- server/clienthandler.py
+++ server/clienthandler.py
@@ -22,9 +22,33 @@ class ClientConnectionHandler(threading.Thread):
         keepChecking = True
 
         while keepChecking:
+            #First, receive a header message
+            header = self.receiveMessage(struct.calcsize(messages.HEADER_STRUCT))
+            header = struct.unpack(messages.HEADER_STRUCT, header)
+
+            payload = self.receiveMessage(struct.calcsize(messages.MESSAGES[header[0]]))
+            if(len(payload) > 0):
+                payload = struct.unpack(messages.MESSAGES[header[0]], payload)
+            else:
+                payload = ()
+            if(header[0] == 3):
+                keepChecking = False
+            self.commandQueue.put((header[0], payload))
+
+        #Still might be something left to send; only close reading buffer.  Socket will be closed by worker thread after sends completed.  
+        self.clientSock.shutdown(socket.SHUT_RD)
+
+    # Gets a message of length bytesToRecv.  Returns bytes of message or None
+    # if message times out.
+    # TODO: Handle Socket errors.  
+    def receiveMessage(self, bytesToRecv):
+        bytesrecvd = b''
+        
+        #Will continue looping until all bytes are received or select times out.  
+        while len(bytesrecvd) < bytesToRecv):
             res = select([self.clientSock], [], [], 30.0)
             if(len(res[0]) == 0):
-                keepChecking = False
-                #Add a message to queue to signal timeout.
-            else:
-                
+                return None
+            bytesrecvd += self.clientSock.recv(bytesToRecv-len(bytesrecvd))
+            
+        return bytesrecvd
--- server/messages.py
+++ server/messages.py
@@ -2,6 +2,15 @@
 # OpenRS v 0.1.0
 # Author: William Barr
 
+## Header message is in network format;
+## Message ID (Unsigned short) followed by unsigned int message len. 
 
 HEADER_STRUCT = "!HI"
 
+#MESSAGE IDs:
+#ID 0: Heartbeat message; no payload.
+#ID 1: Login (Username)
+#ID 2: Login (Password & Token)
+#ID 3: Close Session
+
+MESSAGES = ("", "!30s", "!64sI", "")
